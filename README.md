6fc6797ec20ca4fb6cb5eb0690ed6341cb1de947
thebriankays <brian.kays@gmail.com>
2023-08-06 16:37:06 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Overview
+
+This repository contains a set of scripts that helps setting up my development machine.
+
+## PowerShell
+
+My development environment is heavily centered around using the terminal.
+From a Windows PowerShell prompt, run the following command:
+
+This will install:
+
+- PowerShell Core 7.* _daily_ (pwsh).
+- [Nerd Fonts](https://www.nerdfonts.com/) modified [Cascadia Code](https://docs.microsoft.com/en-us/windows/terminal/cascadia-code) monospaced font to support prompt customizations like powerline or oh-my-posh.
+- [Oh-My-Posh](https://ohmyposh.dev/) PowerShell module.
+- [Terminal-Icons](https://github.com/devblackops/Terminal-Icons) PowerShell module.
+- [customizations](https://github.com/springcomp/powershell_profile.ps1/blob/master/Microsoft.PowerShell_psreadline-profile.ps1) to [PSReadLine](https://docs.microsoft.com/en-us/powershell/module/psreadline/about/about_psreadline?view=powershell-7.2) PowerShell module.
+
+![](bootstrap/assets/powershell.png)
+
+**Warning**: this script is designed to setup a new machine and installs a custom PowerShell profile. Please backup your existing `$profile` file before running this script.
---
+++ setup.ps1
@@ -0,0 +1,90 @@
+Function Invoke-RemoteScript {
+	[CmdletBinding()]
+	param(
+		[Parameter(Position = 0)]
+		[string]$address,
+		[Parameter(ValueFromRemainingArguments = $true)]
+		$remainingArgs
+	)
+
+	iex "& { $(irm $address) } $remainingArgs"
+}
+
+Function Install-OhMyPosh {
+    [CmdletBinding()]
+    param()
+
+    . winget install JanDeDobbeleer.OhMyPosh -s winget
+}
+
+Function Install-TerminalIcons {
+    [CmdletBinding()]
+    param()
+
+    Install-Module -Name Terminal-Icons -Repository PSGallery
+}
+
+Function Install-PSReadLine {
+    [CmdletBinding()]
+    param()
+
+    Install-Module PSReadLine -AllowPrerelease -Force
+}
+
+Function Install-NerdFonts {
+
+    BEGIN {
+        $address = "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip"
+        $archive = "$($Env:TEMP)\CascadiaCode.zip"
+        $folder = "$($Env:TEMP)\CascadiaCode"
+
+        $shell = New-Object -ComObject Shell.Application
+        $obj = $shell.Namespace(0x14)
+        $systemFontsPath = $obj.Self.Path
+    }
+
+    PROCESS {
+
+        Invoke-RestMethod `
+            -Method Get `
+            -Uri $address `
+            -OutFile $archive
+
+        Expand-Archive `
+            -Path $archive `
+            -DestinationPath $folder `
+            -Force
+
+        $shouldReboot = $false
+        
+        Get-ChildItem `
+            -Path $folder |% {
+                $path = $_.FullName
+                $fontName = $_.Name
+                
+                $target = Join-Path -Path $systemFontsPath -ChildPath $fontName
+                if (test-path $target) {
+                    Write-Host "Ignoring $($path) as it already exists." -ForegroundColor DarkGray
+                } else {
+                    Write-Host "Installing $($path)..." -ForegroundColor Cyan
+                    $obj.CopyHere($path)
+                }
+            }
+    }
+
+    END{
+        Remove-Item `
+            -Path $folder `
+            -Recurse `
+            -Force `
+            -EA SilentlyContinue
+    }
+
+}
+
+Set-Alias -Name irs -Value Invoke-RemoteScript
+
+Install-NerdFonts
+Install-OhMyPosh
+Install-TerminalIcons
+Install-PSReadline
